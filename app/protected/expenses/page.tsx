920178cba7010f4e364088080fca75a568ed0575
Riviere Fougy <77861258+rfougy@users.noreply.github.com>
2025-11-15 10:22:50 +0100

    Feat: Supabase Data Fetching for Expenses & Income (#2)
    * feat: create income & expenses pages, allow any globally
    
    * refactor: include account & user data for transactions and expenses
    
    * chore: install prettier
    
    * chore: prettier formatting for all files
    
    * refactor: filter queries by category

---
+++ app/protected/expenses/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { createClient } from '@/lib/supabase/client';
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const [expenses, setExpenses] = useState<any[] | null>(null);
+
+  const supabase = createClient();
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data, error } = await supabase
+        .from('transactions')
+        .select(
+          `
+            *,
+            account:account_id (
+              id,
+              title
+            ),
+            user:user_id (
+              id,
+              first_name
+            )      
+          `
+        )
+        .neq('category', 'income');
+
+      if (error) console.error('Error fetching transactions:', error);
+
+      setExpenses(data);
+    };
+    getData();
+  }, []);
+
+  return <pre>{JSON.stringify(expenses, null, 2)}</pre>;
+}
---
+++ app/protected/income/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { createClient } from '@/lib/supabase/client';
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const [income, setIncome] = useState<any[] | null>(null);
+
+  const supabase = createClient();
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data, error } = await supabase
+        .from('transactions')
+        .select(
+          `
+            *,
+            account:account_id (
+              id,
+              title,
+              distribution_percentage
+            ),
+            user:user_id (
+              id,
+              first_name
+            )      
+          `
+        )
+        .eq('category', 'income');
+
+      if (error) console.error('Error fetching transactions:', error);
+
+      setIncome(data);
+    };
+    getData();
+  }, []);
+
+  return <pre>{JSON.stringify(income, null, 2)}</pre>;
+}
